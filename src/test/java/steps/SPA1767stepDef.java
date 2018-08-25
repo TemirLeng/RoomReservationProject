a55d947e53ff72aedc8bfa4992e80e344f2c3fac
DarinaKasiura <darina.kasyura95@gmail.com>
2018-08-25 13:21:30 -0500

    Darina's feature files and stepDef

---
+++ src/test/java/steps/SPA1767stepDef.java
@@ -0,0 +1,31 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import java.util.List;
+
+public class SPA1767stepDef {
+    @Given("user is on the home page")
+    public void user_is_on_the_home_page() {
+
+    }
+
+    @When("user clicks on schedule")
+    public void user_clicks_on_schedule() {
+
+    }
+
+    @Then("first five hours should match the following:")
+    public void first_five_hours_should_match_the_following(List<String> dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+
+    }
+}
---
+++ src/test/java/steps/SPA1768stepDef.java
@@ -0,0 +1,24 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+import java.util.List;
+
+public class SPA1768stepDef {
+    @Given("user signs in")
+    public void user_signs_in() {
+
+    }
+
+    @Then("user should be able to see links:")
+    public void user_should_be_able_to_see_links(List<String> dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+    }
+}
