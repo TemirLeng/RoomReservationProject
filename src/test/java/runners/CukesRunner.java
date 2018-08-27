1d5331b7ec39e19b832907389c2d2465de83c4aa
Amer <38387759+amerj22@users.noreply.github.com>
2018-08-26 20:35:41 -0500

    Update CukesRunner.java

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -9,8 +9,7 @@ import org.junit.runner.RunWith;
 
         plugin = {"html:target/cucumber", "json:target/report.json"},
         features = "src/test/features",
-        glue = "steps",
-        tags = "@SPA1016"
+        glue = "steps"
 )
 
 public class CukesRunner {
