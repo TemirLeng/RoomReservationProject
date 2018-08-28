bbd88c69695070b9fca6f620b38e8ec6eb598238
DarinaKasiura <darina.kasyura95@gmail.com>
2018-08-27 19:30:12 -0500

    json direction

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = {"html:target/cucumber", "json:target/report.json"},
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
         features = "src/test/features",
         glue = "steps"
 
